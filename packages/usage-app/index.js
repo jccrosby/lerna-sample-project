f2c3147fced62ad77fcf8870f50bb83751ca8b1f
John Crosby <john.crosby@mlb.com>
2019-04-16 09:52:42 -0400

    BREAKING CHANGE: Introduced a breaking change
    Nothing scary, just some additional output

--- packages/usage-app/index.js
+++ packages/usage-app/index.js
@@ -2,3 +2,5 @@ const componentOne = require('component-one/index');
 const componentTwo = require('component-two/index');
 
 console.log('Added a new feature');
+
+console.log('Introduce a breaking change');
