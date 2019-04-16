928816e0553ad94adf088afdce39dcafd6c4ea1c
John Crosby <jccrosby@gmail.com>
2019-04-16 09:54:15 -0400

    BREAKING CHANGE: Introduced a breaking change
    * feat: added a new feature
    * BREAKING CHANGE: Introduced a breaking change
      * Nothing scary, just some additional output

--- packages/usage-app/index.js
+++ packages/usage-app/index.js
@@ -1,2 +1,6 @@
 const componentOne = require('component-one/index');
 const componentTwo = require('component-two/index');
+
+console.log('Added a new feature');
+
+console.log('Introduce a breaking change');
